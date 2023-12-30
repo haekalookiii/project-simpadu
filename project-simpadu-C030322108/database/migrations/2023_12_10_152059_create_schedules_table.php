305a852d183df02b67c388ce5200647acc38102b
haekalookiii <bimahaekal312@gmail.com>
2023-12-30 18:05:15 +0800

    project-simpadu-c030322108

---
+++ project-simpadu-C030322108/database/migrations/2023_12_10_152059_create_schedules_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('subject_id')->unsigned();
+            $table->string('hari');
+            $table->string('jam_mulai');
+            $table->string('jam_selesai');
+            $table->string('ruangan');
+            $table->string('kode_absensi');
+            $table->string('tahun_akademik');
+            $table->string('semester');
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->string('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
---
+++ project-simpadu-C030322108/database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory(25)->create();
+
+        User::create([
+            'name' => 'Bima Haekal Akbar',
+            'email' => 'c030322108@mahasiswa.poliban.ac.id',
+            'email_verified_at' => now(),
+            'password' => Hash::make('c030322108'),
+            'roles' => 'mahasiswa',
+        ]);
+    }
+}
