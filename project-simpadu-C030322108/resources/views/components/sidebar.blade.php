2dc4c54858cdcb547d3fd7a37a4d3d5efda9a4d4
haekalookiii <bimahaekal312@gmail.com>
2024-01-02 19:22:31 +0800

    project-simpadu-c030322108

--- project-simpadu-C030322108/resources/views/components/sidebar.blade.php
+++ project-simpadu-C030322108/resources/views/components/sidebar.blade.php
@@ -26,6 +26,14 @@
 
                 </ul>
             </li>
+             <li class="nav-item dropdown ">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Subject</span></a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a class="nav-link" href="{{ url('dashboard-general-dashboard') }}">Subject List</a>
+                    </li>
+                </ul>
+            </li>
             <li class="nav-item dropdown ">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Schedule</span></a>
                 <ul class="dropdown-menu">
